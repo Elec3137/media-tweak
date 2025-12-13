c3d3bf2441a020e3ed95e2c3f7cf280e83742d5d
Elec3137 <elec3137@proton.me>
2025-12-13 14:56:54 -0800

    initial repo
    with iced (dep)

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    with pkgs;
+    {
+      packages."x86_64-linux".default = rustPlatform.buildRustPackage {
+        pname = "media-tweak";
+        version = self.shortRev or self.dirtyShortRev;
+        src = ./.;
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+        nativeBuildInputs = [ ];
+        buildInputs = [ ];
+      };
+      
+      devShells."x86_64-linux".default = mkShell {
+        inputsFrom = [ self.packages."x86_64-linux".default ];
+        LD_LIBRARY_PATH = lib.makeLibraryPath [
+          wayland
+          libxkbcommon
+        ];
+      };
+    };
+}
